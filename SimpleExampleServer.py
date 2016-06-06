6cc7830171265988afe0a42ac8d7fcd82fecaac5
heltonbiker <heltonbiker@gmail.com>
2016-06-06 18:57:38 -0300

    RoadScanner.html loading previously found roads
    - from 'roads.txt' file, sent via server.onConnect()

--- SimpleExampleServer.py
+++ SimpleExampleServer.py
@@ -6,36 +6,81 @@ Copyright (c) 2013 Dave P.
 import os
 import signal
 import sys
+from array import array
+import codecs
+import re
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
 
+# handlers to override in WebSocket:
+# handleMessage:   called when websocket frame is received;
+# handleConnected: called when client connects to the server;
+# handleClose:     called when server gets "close" from a client;
+# sendMessage:     send frame to the client
+#
+#
 
-class SimpleEcho(WebSocket):
 
+
+class RoadsSocket(WebSocket):
+
+    _clients = []
     _fname = 'roads.txt'
-
-    def handleMessage(self):
-        print "message"
-
-        if not os.path.exists(self._fname):
-            print "gonna save the file"
-            with open(self._fname, 'w'):
-                pass  
-                
-        with open(self._fname, 'a') as out:
-            out.write(self.data)
-            out.write("\n")
-
-    def handleClose(self):
-        print "close"
 
     def handleOpen(self):
         print "open"
+        print dir(self)
 
+    def handleConnected(self):
+        with open(self._fname) as roadsfile:
+            lines = roadsfile.readlines()
+            for line in lines:
+                numbers = map(float, re.split('[\s,]+', line.strip()))
+                print numbers
+                a = array('d', numbers)       
+                self.sendMessage(a.tostring())
+
+        # print (self.address, 'connected')
+        # for client in clients:
+        #     client.sendMessage(self.address[0] + u' - connected')
+        # clients.append(self)        
+
+    def handleMessage(self):
+        print "message"
+        print dir(self)
+        print self.client
+        print self.data
+
+        # if not os.path.exists(self._fname):
+        #     print "gonna save the file"
+        #     with open(self._fname, 'w'):
+        #         pass  
+                
+        # with open(self._fname, 'a') as out:
+        #     out.write(self.data)
+        #     out.write("\n")
+
+      # for client in clients:
+      #    if client != self:
+      #       client.sendMessage(self.address[0] + u' - ' + self.data)            
+
+    def handleClose(self):
+        print "close"
+        print dir(self)
 
 
 if __name__ == "__main__":
 
-    server = SimpleWebSocketServer("localhost", 8000, SimpleEcho)
+    # with open('roads.txt') as roadsfile:
+
+    #     content = roadsfile.read()
+    #     numbers = map(float, re.split('[\s,].?', content.strip()))
+    #     a = array('d', numbers)
+    #     print a.tostring()
+
+    #     exit()
+
+
+    server = SimpleWebSocketServer("localhost", 8000, RoadsSocket)
 
     print "serving at ws://localhost:8000"
 
